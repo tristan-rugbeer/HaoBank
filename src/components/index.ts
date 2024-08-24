1bd023118b765688ea1e31b87fbc334bcf695fa8
tristan-rugbeer <tristanrugbeer93@gmail.com>
2024-08-24 22:23:38 +0200

    Initial App Commit

---
+++ src/components/index.ts
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CTA from "./CTA";
+import Clients from "./Clients";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Billing from "./Billing";
+
+export {
+    Navbar,
+    Billing,
+    Stats,
+    Testimonials,
+    Hero,
+    Footer,
+    CTA,
+    Clients,
+    CardDeal,
+    Business
+}
